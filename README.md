c532591c206af13714197851e39a0c000b490777
olga-golentova <golenolg@gmail.com>
2025-05-30 16:44:22 +0200

    Create a test for displaying package details
    1. Add a custom command selectByDataTestID()
    2. Write the test in the homepage-spec

---
+++ README.md
@@ -0,0 +1,14 @@
+# UI Automation Test
+
+This project uses Cypress as UI automation tool.
+
+## Prerequisites 
+1. Make sure you have Node.js v18 or later installed
+2. Run `npm install`
+
+## To run tests in headless mode:
+`npm run cypress-run`
+
+## To run tests in GUI:
+`npm run cypress-open`
+
--- cypress/e2e/homepage-spec.cy.js
+++ cypress/e2e/homepage-spec.cy.js
@@ -1,2 +1,39 @@
-describe ('Home Page', () => {
+/// <reference types="cypress" />
+
+describe('Home Page', () => {
+
+    it('should display product details correctly', () => {
+        // Set cookies to prevent redirect to localized page
+        cy.setCookie('Airalo.locale', 'en');
+        cy.setCookie('Airalo.currency', 'USD');
+        cy.setCookie('airalo_browser_language', 'en');
+       
+        cy.visit('https://www.airalo.com/')
+        // Wait up to 10 sec for spinner to dissapear
+        cy.selectByDataTestId('search-hydration-loader', { timeout: 10000 }).should('not.exist')
+        
+        cy.selectByDataTestId('search-input')
+        .click()
+        .type('Japan')
+
+        cy.get('.countries-list-loader').should('not.exist') // Wait until list loaded
+        cy.get('.countries-list').within( () => { // Make sure clicked on the element in the search results list
+            cy.selectByDataTestId('Japan-name')
+            .click()
+        })
+
+
+        cy.url().should('include','/japan-esim')
+        // The first "Buy now" button is the second in the DOM by its data-testId
+        cy.selectByDataTestId('esim-button').eq(1).click() 
+
+        cy.selectByDataTestId('sim-detail-header').should('be.visible')
+        cy.selectByDataTestId('sim-detail-header').within( () => {
+            cy.selectByDataTestId('sim-detail-operator-title').should('contain.text','Moshi Moshi')
+            cy.selectByDataTestId('COVERAGE-value').should('contain.text','Japan')
+            cy.selectByDataTestId('DATA-value').should('contain.text','1 GB')
+            cy.selectByDataTestId('VALIDITY-value').should('contain.text','7 Days')
+            cy.selectByDataTestId('PRICE-value').should('contain.text','$4.50 USD')    
+        }) 
+    })
 })
